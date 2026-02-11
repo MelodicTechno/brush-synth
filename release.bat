160daab5137d01cce34c89fa7323204ee80f493b
MelodicTechno <617179025@qq.com>
2026-02-12 00:55:46 +0800

    update: hide cmd

--- release.bat
+++ release.bat
@@ -5,8 +5,8 @@ set QT_BIN_DIR=E:\qt\6.10.2\mingw_64\bin
 set PATH=%QT_BIN_DIR%;%PATH%
 
 echo [INFO] Setting up Release build environment...
-if not exist build_release mkdir build_release
-cd build_release
+if not exist release mkdir release
+cd release
 
 echo [INFO] Configuring (Release)...
 cmake -G "Ninja" -DCMAKE_BUILD_TYPE=Release ..
@@ -25,7 +25,7 @@ if %ERRORLEVEL% NEQ 0 (
 cd ..
 if not exist release mkdir release
 echo [INFO] Copying executable to release folder...
-copy /Y build_release\brush-synth.exe release\brush-synth.exe
+copy /Y release\brush-synth.exe release\brush-synth.exe
 
 cd release
 echo [INFO] Deploying Qt dependencies...
--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,7 @@
 #include <QApplication>
 #include "MainWindow.h"
-#ifdef _WIN32
-#include <windows.h>
-#endif
 
 int main(int argc, char *argv[]) {
-#ifdef _WIN32
-    // Hide console window
-    FreeConsole();
-#endif
     QApplication app(argc, argv);
     
     MainWindow w;
